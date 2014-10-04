71c59a94703d57b1796d4c670297214a1d819821
awallace87 <andrew.f.wallace@gmail.com>
2014-10-04 16:03:16 -0400

    Format Grade Worksheets for Moodle
    Toolset of python scripts to format cuLearn CSV grade worksheets for
    easier compilation of grades and comments

---
+++ Add Question Fields/AddQuestionFields.py
@@ -0,0 +1,58 @@
+import csv
+import glob
+import os
+
+outputDir = "Output"
+columnsToInclude = [    "Identifier"
+                      , "Full name"
+                      , "Status"
+                      , "Grade"
+                      , "Maximum Grade"
+                      , "Feedback comments"]
+
+#Ask how many question fields you would like to add
+print "Add Question Columns Tool"
+print "This script will add the appropriate columns to the CULearn CSV file for quick formatting"
+
+#Find CSV Files beginning with Grades-
+gradeCSVFiles = glob.glob("Grades-*.csv")
+
+if gradeCSVFiles.count <= 0:
+  print "No Grade CSV files found in directory\nPress any key to exit"
+  raw_input()
+else:
+  print "How many questions are you grading for this assignment?"
+  numOfQuestions = int(raw_input())
+  print "Will add {0} Question Columns\n".format(numOfQuestions)
+
+  print "How many bonus questions are in the assignment?"
+  numOfBonus = int(raw_input())
+  print "Will add {0} Bonus Columns\n".format(numOfBonus)
+
+  for name in gradeCSVFiles:
+    print "Analysing grade files"
+    csvFileReader = csv.DictReader(open(name ,'rb'), delimiter=",")
+    print "Field Names Before: " + str(csvFileReader.fieldnames)
+    for questionNum in range(1,numOfQuestions + 1):
+      questionCommentString = "Question {0} Comments".format(questionNum)
+      csvFileReader.fieldnames.append(questionCommentString)
+      columnsToInclude.append(questionCommentString)
+      questionDeductString = "Question {0} Deductions".format(questionNum)
+      csvFileReader.fieldnames.append(questionDeductString)
+      columnsToInclude.append(questionDeductString)
+    for bonusNum in range(1,numOfBonus + 1):
+      bonusCommentString = "Bonus {0} Comments".format(bonusNum)
+      csvFileReader.fieldnames.append(bonusCommentString)
+      columnsToInclude.append(bonusCommentString)
+      bonusDeductString = "Bonus {0} Grades".format(bonusNum)
+      csvFileReader.fieldnames.append(bonusDeductString)
+      columnsToInclude.append(bonusDeductString)
+    print "Field Names After: " + str(csvFileReader.fieldnames)
+    print "Columns To Include: " + str(columnsToInclude)
+    outputFilename = os.path.splitext(name)[0] + "-output.csv"
+    outputFile = open(os.path.join(outputDir, outputFilename), "wb")
+    csvFileWriter = csv.DictWriter(outputFile, delimiter=",", fieldnames=csvFileReader.fieldnames)
+    csvFileWriter.writeheader()
+    for row in csvFileReader:
+      csvFileWriter.writerow(row) #[col] for col in columnsToInclude)
+    outputFile.close()
---
+++ Grade Formatter/GradeFormatter.py
@@ -0,0 +1,74 @@
+#Grade Formatter
+#Compiles comments and deductions for individual questions
+#Outputs formatted final comments and grades
+import glob
+import os
+import sys
+import csv
+
+outputDir = "Output"
+outputPath = "-formatted.csv"
+
+questionSubstring = "Question"
+deductionsFormat = " - Deductions"
+commentsSubstring = " - Comments"
+
+
+def scan_and_write():
+  gradeCSVFiles = glob.glob("Grades-*.csv")
+
+  if gradeCSVFiles.count <= 0:
+    print "No Grade CSV files found in directory\nPress any key to exit"
+    raw_input()
+  else:
+    print "How many questions are you grading for this assignment?"
+    numOfQuestions = int(raw_input())
+
+    print "How many bonus questions are in the assignment?"
+    numOfBonus = int(raw_input())
+
+    for name in gradeCSVFiles:
+      print name
+      csvFileObj = open(name, 'rb')
+      reader = csv.DictReader(csvFileObj, delimiter=',')
+      outputFilename = os.path.splitext(name)[0] + "-formatted.csv"
+      outputPath = os.path.join(outputDir,outputFilename)
+      csvOutputFile = open(outputPath, "wb")
+      csvWriter = csv.DictWriter(csvOutputFile, delimiter=',' , fieldnames=reader.fieldnames)
+      csvWriter.writeheader()
+      for line in reader:
+        compiledFeedback = ""
+        compiledGrade = float(line["Maximum Grade"])
+        for num in range(1, numOfQuestions + 1):
+          #try:
+          questionHeader = "[Question {0}]".format(num)
+          questionComments = line["Question {0} Comments".format(num)]
+          questionDeductions = line["Question {0} Deductions".format(num)]
+          if str(questionComments) != "":
+            compiledFeedback += questionHeader + " " + questionComments
+          if questionDeductions != "" and float(questionDeductions) < 0:
+            compiledGrade += float(questionDeductions)
+            if str(questionComments) == "":
+              compiledFeedback += questionHeader
+            compiledFeedback += "({0} Marks)\n".format(questionDeductions)
+          #except:
+          #  print "Unable to find Question {0} information".format(num)
+        for bonusNum in range(1, numOfBonus + 1):
+          #try:
+          bonusQuestionHeader = " [Bonus Question {0}]".format(bonusNum)
+          bonusQuestionComments = line["Bonus {0} Comments".format(bonusNum)]
+          bonusQuestionRewards = line["Bonus {0} Grades".format(bonusNum)]
+          if str(bonusQuestionComments) != "":
+            compiledFeedback += bonusQuestionHeader + " " + bonusQuestionComments
+          if bonusQuestionRewards != "" and float(bonusQuestionRewards) > 0:
+            compiledGrade += float(bonusQuestionRewards)
+            compiledFeedback += "(+{0} Marks)\n".format(bonusQuestionRewards)
+          #except:
+          #  print "Unable to find Bonus Question {0} information".format(bonusNum)
+        line["Grade"] = compiledGrade
+        line["Feedback comments"] = (compiledFeedback)
+        csvWriter.writerow(line)
+      csvOutputFile.close()
+
+if __name__ == "__main__":
+  scan_and_write()
